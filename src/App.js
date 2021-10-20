5a133942d5b2262f5deac2220ab183e25f907fb3
Jase Kraft <jase@perfectmypodcast.com>
2021-10-20 12:46:16 -0500

    completed login-registration UI

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import logo from './logo.svg';
-import Header from './components/header/header'
+import Login from './components/Login/login'
 import Game from './components/game/game'
 import './App.css';
 import gameData from './SampleGame'
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-        <Header />
-        <Game game={gameData} />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+        loggedIn: false,
+        gameData: gameData,
+    }
+  }
+  renderApp(){
+    if(this.state.loggedIn){
+      return(<Game game={gameData} />)
+    }
+    else{
+      return(< Login />)
+    }
+  }
+  render(){
+    return (
+      <div className="App">
+         { this.renderApp()}
+      </div>
+    );
+  }
 }
 
 export default App;
